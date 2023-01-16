a95ad77faa8e22dc07fea28456b663347c393eee
marcelonicacio <marcelonicacio@me.com>
2023-01-16 22:56:16 +0000

    layout finalizado e dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
 import Head from 'next/head'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
-import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub,AiFillHtml5} from 'react-icons/ai'
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub,AiFillHtml5, AiFillFacebook} from 'react-icons/ai'
 import {FaCss3Alt, FaReact, FaPython , FaGithub, FaNodeJs} from 'react-icons/fa'
 import { IoLogoJavascript } from "react-icons/io5"
 import { SiArduino, SiMysql } from "react-icons/si";
 import Image from 'next/image'
-import nica from '../public/nica.JPG'
-import simon from '../public/telasimon.png';
-import credit from '../public/telacredit.png';
-import movies from '../public/telamovies.png';
-import React from 'react';
+import nica from '../src/img/marcenica1.png'
+import simon from '../src/img/telasimon.png';
+import credit from '../src/img/telacredit.png';
+import movies from '../src/img/telamovies.png';
+import python from '../src/img/python.png';
 import { ContactUs } from '../src/components/Contact'; 
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 
 export default function Home() {
 const [darkMode, setDarkMode] = useState(false);
-
-
 
   return (
     <div className={darkMode ? 'dark' : ''}>
@@ -27,111 +25,139 @@ const [darkMode, setDarkMode] = useState(false);
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=' bg-gray-200 px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
-        <section className='min-h-screen'>
+      <main>
+        <section className='min-h-screen layout dark:bg-gray-700'>
           <nav className='py-10 mb-20 flex justify-between'>
-            <h1 className='text-xl font-burtons'>marcelo nicacio</h1>
-            <ul className='flex items-center'>
-              <li>
-                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl dark:text-gray-300 scale-x-[-1]'/>
+            <div></div>
+            <ul className='flex items-center px-10 md:px-20 lg:px-40 '>
+              <li >
+                <BsFillMoonStarsFill  onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl scale-x-[-1] dark:text-yellow-400'/>
               </li>
-              <li><a className='typing bg-gradient-to-r from-blue-300 to-blue-400 text-black shadow-md px-4 py-2 rounded-md ml-8' href="#">Contact</a></li>
+              <li><a className='amarelo text-black text-sm shadow-md px-3 py-2 ml-8 font-semibold' href="#contact">CONTACT</a></li>
             </ul>
           </nav>
           
             <div className='flex flex-col justify-center items-center h-96'>
               <div>
-                <h2 className='text-4xl md:text-6xl xl:text-7xl my-5 pt-10 dark:text-white'>Hi ,</h2>
-                <h2 className='text-4xl md:text-6xl xl:text-7xl dark:text-white'> I'm <span className='bg-sky-400 shadow-lg text px-5 text-white dark:text-black'>MARCELO NICACIO</span></h2>
+                <h2 className='text-4xl md:text-6xl xl:text-7xl my-5 pt-10 font-extrabold text-white drop-shadow-sm shadow-black/50'>Hi ,</h2>
+                <h2 className='text-4xl font-extrabold md:text-6xl xl:text-7xl text-white drop-shadow-sm shadow-black/50'> I'm <span className='amarelo shadow-lg text-black px-5  dark:text-black'>MARCELO NICACIO</span></h2>
               </div>
-              <h3 className='text:xl lg:text-2xl my-4 dark:text-white'>Developer Front-End</h3>
-              <div className=' text-3xl flex justify-center gap-16 py-5 text-gray-600 dark:text-gray-400'>
-            <AiFillTwitterCircle />
-            <AiFillLinkedin />
-            <AiFillGithub />
+              <h3 className='text:xl lg:text-2xl my-4 font-semibold'>Developer Front-End</h3>
+              <div className=' text-4xl flex justify-center gap-16 py-5 text-white  dark:text-white'>
+            <AiFillLinkedin className='cursor-pointer hover:text-black'/>
+            <AiFillGithub className='cursor-pointer hover:text-black' />
+            <AiFillTwitterCircle className='cursor-pointer hover:text-black' />
            
           </div>
             </div>
           
         </section>
-        <section className='  grid grid-cols-1 lg:grid-cols-2 items-center' >
-          <div className='max-w-lg py-5 my-20 mx-auto border-spacing-2  shadow-xl bg-blue-400 rounded-md flex flex-col items-center space-x-4'>
-            <h3 className='text-3xl '>About me</h3>
-            <p className='text-md py-2 dark:text-gray-200 leading-8 text-gray-800 md:text-xl mx-auto'>Developer Front-end based in Porto, Portugal and robotic lover.</p>
-            <h4 className='text-4xl m-5' >Skills</h4>
+        <section className='min-h-screen bg-white dark:bg-gray-700 flex justify-around' >
+
+          <div className=' amarelo w-1/4 dark:bg-gray-800 flex flex-col items-center justify-center'>
             <div className=''>
-              <ul className='text-6xl grid grid-cols-3 gap-6'>
-                <li alt='HTML'><AiFillHtml5/></li>
-                <li><FaCss3Alt/></li>
-                <li><IoLogoJavascript/></li>
-                <li><FaReact/></li>
-                <li><FaNodeJs/></li>
-                <li><FaPython/></li>
-                <li><FaGithub/></li>
-                <li><SiArduino/></li>
-                <li><SiMysql/></li>
+              <h3 className='text-3xl text-white text-shadow-md font-bold text-center'>Marcelo Nicacio</h3>
+              <p className='text-sm mb-5 py-2 text-gray-700 font-semibold  dark:text-gray-200 leading-8'>Developer Front-end based in Porto, Portugal.</p>
+            </div>
+          
+            <div className=' max-w-lg '>
+              <Image className='w-72 shadow-md rounded-full' src={nica} alt={'foto marcelo'}/>
+              </div>
+            
+          </div> 
+          <div className='  w-3/4 flex px-60 flex-col p-20 justify-center items-center'>
+            <div className='text-lg border-black border-2 py-1 px-2 shadow-lg dark:text-white dark:border-white font-semibold mb-5'>#aboutMe</div>
+          <p className=' dark:text-gray-200 text-gray-800 md:text-md  mx-auto'>Brazilian, graduated in Marketing, estudo front end há alguns anos e tenho um pouco de conhecimento em back-end, além disso me interesso por games, robótica e deep learning. </p>
+          
+          <h4 className='text-5xl m-10 font-extrabold dark:text-white' >Skills</h4>
+           
+              <ul className='text-6xl   grid grid-cols-3 gap-10'>
+                <li alt='HTML'><AiFillHtml5 className='text-orange-600'/></li>
+                <li><FaCss3Alt className='text-blue-500'/></li>
+                <li><IoLogoJavascript className='text-yellow-400'/></li>
+                <li><FaReact className='text-sky-700 '/></li>
+                <li><FaNodeJs className='text-lime-600'/></li>
+                <li><Image src={python} className='h-16 w-16'/></li>
+                <li><FaGithub className='dark:text-white'/></li>
+                <li><SiArduino className='text-teal-500'/></li>
+                <li><SiMysql className='dark:text-white'/></li>
               </ul>
             </div>
-          </div> 
-         
-            <div className='shadow-lg bg-green-300  max-w-lg mx-auto rounded-full overflow-hidden md:h-96 md:w-96'>
-              <Image src={nica} alt={'foto marcelo'}/>
-              </div>
-          
+
+   
   </section>
-  <section className='min-h-screen'>
+  <section className='min-h-screen dark:bg-gray-700 layout px-10 md:px-20 lg:px-40 '>
 
-    <div ><h2 className='text-5xl my-10'>Projects</h2></div>
+    <div ><h2 className='text-5xl text-white font-semibold py-20'>Projects</h2></div>
 
-        <div className='grid-cols-1 grid md:grid-cols-2 md:gap-5 lg:grid-cols-3' >
-            <div className='mb-20 flex flex-col justify-between text-start text-sm overflow-hidden max-w-xs h-96 bg-gray-400 shadow-lg  rounded-2xl dark:bg-white'>
+        <div className='grid-cols-1 grid md:grid-cols-2 md:gap-10 px-20 dark:bg-gray-700 lg:grid-cols-3' >
+            <div className='mb-20 flex flex-col justify-between text-start text-sm overflow-hidden max-w-xs h-96 bg-white shadow-xl rounded-md dark:bg-gray-800'>
               <div className='max-w-md'>
-                <Image className='object-contain cursor-pointer' src={simon} alt={'imagem projeto simon'} />
+                <Image className='object-cover cursor-pointer' src={simon} alt={'imagem projeto simon'} />
               </div>
-              <h3 className='text-lg font-medium p-2'>Simon Says!</h3>
-              <p className='p-2 text-gray-700'>This project simula the classic toy Simon, the game have the same nostalgic colors and sounds. 
+              <h3 className='text-lg px-5 font-medium p-2 dark:text-gray-200'>Simon Says!</h3>
+              <p className='p-2 text-gray-700 dark:text-gray-200 px-5'>This project simula the classic toy Simon, the game have the same nostalgic colors and sounds. 
               </p>
-              <h4 className='p-2 text-sky-700'>Technologies used...</h4>
+              <h4 className='p-2 text-bold-700 font-semibold dark:text-gray-200'>Technologies used...</h4>
               <div className='flex justify-around gap-1 items-center p-2' >
-               <span className='bg-gray-500 text-xs rounded-xl px-2 text-white'>HTML</span><span className='bg-gray-500 text-xs rounded-xl px-2 text-white'>CSS</span><span className='bg-gray-500 text-xs rounded-xl px-2 text-white'>JavaScript</span>
-               <span className='bg-gray-700 rounded-lg text-xs px-2 text-white cursor-pointer'>View More!</span>
+               <span className='bg-orange-500 text-semibold text-xs rounded-xl px-2 text-white'>HTML</span><span className='bg-blue-500 text-semibold text-xs rounded-xl px-2 text-white'>CSS</span><span className='bg-yellow-400 text-semibold text-xs rounded-xl px-2 text-white'>JavaScript</span>
+               <span className='bg-gray-700 text-xs px-2 text-white cursor-pointer'>View More!</span>
               </div>
             
             </div>
             
-            <div className='text-start mb-20 flex flex-col justify-between  text-sm overflow-hidden max-w-xs h-96  bg-gray-400 shadow-lg  rounded-2xl dark:bg-white'>
-              <div className='max-w-xl  '>
-                <Image className='object-contain w-96 cursor-pointer' src={credit} alt={'imagem projeto rocket card'} />
+            <div className='mb-20 dark:bg-gray-800 flex flex-col justify-between text-start text-sm overflow-hidden max-w-xs h-96 bg-white shadow-xl  rounded-md'>
+              <div className='max-w-md'>
+                <Image className='object-cover w-96 cursor-pointer' src={credit} alt={'imagem projeto rocket card'} />
               </div>
-              <h3 className='text-lg font-medium p-2'>Rocket Card</h3>
-              <p className='p-2 text-gray-700'>Credit card project with Regular expression to identify the card company.</p>
-              <h4 className='p-2 text-sky-700'>Technologies used...</h4>
+              <h3 className='text-lg font-medium px-5 p-2 dark:text-gray-200'>Rocket Card</h3>
+              <p className='p-2 px-5 dark:text-gray-200  text-gray-700'>Credit card project with Regular expression to identify the card company.</p>
+              <h4 className='p-2 text-gray-700 font-semibold dark:text-gray-200'>Technologies used...</h4>
               <div className='flex justify-around gap-1 items-center p-2' >
-               <span className='bg-gray-500 text-xs rounded-xl px-2 text-white'>CSS</span><span className='bg-gray-500 text-xs rounded-xl px-2 text-white'>React JS</span><span className='bg-gray-500 text-xs rounded-xl px-2 text-white'>JavaScript</span>
-               <span className='bg-gray-700 rounded-lg text-xs px-2 text-white cursor-pointer'>View More!</span>
+               <span className='bg-blue-500 text-xs rounded-xl px-2 text-white'>CSS</span><span className='bg-blue-700 text-xs rounded-xl px-2 text-white'>React JS</span><span className='bg-yellow-400 text-xs rounded-xl px-2 text-white'>JavaScript</span>
+               <span className='bg-gray-700 text-xs px-2 text-white cursor-pointer'>View More!</span>
               </div>
             
             </div>
-            <div className='text-start mb-20 flex flex-col justify-between  h-96 text-sm max-w-xs overflow-hidden  bg-gray-400 shadow-lg rounded-2xl dark:bg-white'>
-              <div className='max-w-xl  '>
-                <Image className='object-contain cursor-pointer w-96' src={movies} alt={'imagem projeto movies'} />
+            <div className='mb-20 dark:bg-gray-800 flex flex-col justify-between text-start text-sm overflow-hidden max-w-xs h-96 bg-white shadow-xl  rounded-md '>
+              <div className='max-w-md'>
+                <Image className='object-contain cursor-pointer w-96 border-gray-400' src={movies} alt={'imagem projeto movies'} />
               </div>
-              <h3 className='text-lg font-medium p-2'>Best Movies IMDB</h3>
-              <p className='p-2 text-gray-700'>This project show the best movies order list on Imdb and description about each movie.</p>
-              <h4 className='p-2 text-sky-700'>Technologies used...</h4>
+              <h3 className='text-lg font-medium p-2 dark:text-gray-200'>Best Movies IMDB</h3>
+              <p className='p-2 dark:text-gray-200  text-gray-700'>This project show the best movies order list on Imdb and description about each movie.</p>
+              <h4 className='p-2 text-gray-700 font-semibold dark:text-gray-200'>Technologies used...</h4>
               <div className='flex justify-around gap-1 items-center p-2' >
-               <span className='bg-gray-500 text-xs rounded-xl px-2 text-white'>React JS</span><span className='bg-gray-500 text-xs rounded-xl px-2 text-white'>CSS</span><span className='bg-gray-500 text-xs rounded-xl px-2 text-white'>JavaScript</span>
-               <span className='bg-gray-700 rounded-lg text-xs px-2 text-white cursor-pointer'>View More!</span>
+               <span className='bg-blue-700 text-xs rounded-xl px-2 text-white'>React JS</span><span className='bg-blue-500 text-xs rounded-xl px-2 text-white'>CSS</span><span className='bg-yellow-400 text-xs rounded-xl px-2 text-white'>JavaScript</span>
+               <span className='bg-gray-700 text-xs px-2 text-white cursor-pointer'>View More!</span>
               </div>
             
             </div>
           </div>
         </section> 
-        <section className='min-h-screen flex items-center justify-center bg-blue-400'>
-          <ContactUs/>
+        <section className='min-h-screen layout flex flex-col justify-between dark:bg-gray-700  '>
+
+          <div className=' flex flex-row justify-around items-center z-10'>
+            <div className='pt-60'>
+              <h2 className='text-yellow-400 text-xl font-semibold mt-30 mb-4 dark:text-yellow-400'>Contact</h2>
+              <h3 className='text-7xl font-bold dark:text-white'>Let's talk</h3>
+              <div className=' text-4xl flex justify-center gap-16 py-5 text-white  dark:text-gray-900'>
+            <AiFillLinkedin className='cursor-pointer hover:text-black'/>
+            <AiFillFacebook className='cursor-pointer hover:text-black' />
+            <AiFillTwitterCircle className='cursor-pointer hover:text-black' />
+           
+          </div>
+            </div>
+            <div id='contact' className='relative top-48 right-24 bg-gray-400 dark:bg-gray-500 '>
+              <h2 className='yb-5 py-5 pl-2 bg-gray-500 dark:bg-black font-bold text-shadow-md text-white  text-lg'>Send me a Message</h2>
+              <ContactUs/>
+            </div>
+          </div>
+          <div className='bg-white dark:bg-gray-800 h-48 w-full'>
+          </div>
         </section>
+     
       </main>
-      <footer className='flex justify-center items-center'><h4>Marcelo Nicacio - 2023</h4></footer>
+  
     </div>
   )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,17 +18,16 @@ export const ContactUs = () => {
   };
 
   return (
-    <div className='bg-white w-96 h-96 border-spacing-2'>
-        <h2 className='mb-5'>Send me a Message</h2>
-        <form className='flex flex-col p-2 w-96 bg-gray-200 rounded-md max-w-md' ref={form} onSubmit={sendEmail}>
+      
+        <form className='flex flex-col p-2 w-96 shadow-md  max-w-md' ref={form} onSubmit={sendEmail}>
           <label>Name</label>
-          <input type="text" name="user_name" />
+          <input type="text" name="from_name" />
           <label>Email</label>
           <input type="email" name="user_email" />
           <label >Message</label>
-          <textarea name="message"  />
-          <input type="submit" value="Send" />
+          <textarea className='mb-4' rows={6} name="message"  />
+          <button type="submit" value="Send" id='message' className=' w-20 h-5 shadow-md'>Send</button>
         </form>
-    </div>
+
   );
 };

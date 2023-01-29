78c03a688fccfbb7660c3534575a4481868ec5ed
marcelonicacio <marcelonicacio@me.com>
2023-01-29 00:27:50 +0000

    add fonts

--- pages/index.js
+++ pages/index.js
@@ -41,10 +41,10 @@ const [darkMode, setDarkMode] = useState(false);
           
             <div className='flex flex-col justify-center items-center h-96'>
               <div>
-                <h2 className='text-4xl md:text-6xl xl:text-7xl my-5 pt-10 font-extrabold text-white drop-shadow-sm shadow-black/50'>Hi ,</h2>
-                  <h2 className='text-4xl font-extrabold md:text-6xl xl:text-7xl text-white drop-shadow-sm shadow-black/50'> I'm <span className='amarelo shadow-lg text-black px-5  dark:text-black'>MARCELO NICACIO</span></h2>
+                <h2 className='text-4xl md:text-6xl xl:text-7xl my-5 pt-10 font-extrabold text-white drop-shadow-sm shadow-black/50'></h2>
+                  <h2 className='text-4xl font-extrabold md:text-6xl xl:text-7xl text-white drop-shadow-sm shadow-black/50'><span className='amarelo shadow-lg text-black px-5  dark:text-black'>MARCELO NICACIO</span></h2>
               </div>
-              <h3 className='text:xl lg:text-2xl my-4 dark:text-gray-200 font-semibold'>Developer Front-End</h3>
+              <h3 className='text:xl lg:text-2xl my-4 dark:text-gray-200 font-semibold'>Desenvolvedor Front-End</h3>
               <div className=' text-4xl flex justify-center gap-16 py-5 text-white  dark:text-white'>
             <span>
               <AiFillLinkedin className='cursor-pointer hover:text-black'/><a href="https://www.linkedin.com/in/marcelo-nicacio/"></a>
@@ -60,35 +60,35 @@ const [darkMode, setDarkMode] = useState(false);
             </div>
           
         </section>
-        <section className='min-h-screen bg-white dark:bg-gray-700 flex justify-around' >
+        <section className='min-h-screen bg-white dark:bg-gray-600 md:flex justify-between' >
 
-          <div className=' bg-gray-400 w-1/4 dark:bg-gray-800 flex flex-col items-center justify-center'>
-            <div className=''>
+          <div className=' bg-gray-400 md:w-1/4 dark:bg-gray-800 flex flex-col items-center justify-center'>
+            <div>
               <h3 className='text-3xl text-white text-shadow-md font-bold text-center'>Marcelo Nicacio</h3>
-              <p className='text-sm mb-5 py-2 text-gray-700 font-semibold  dark:text-gray-200 leading-8'>Developer Front-end based in Porto, Portugal.</p>
+              <p className='text-sm md:mb-5 md:py-2 text-gray-700 font-semibold  dark:text-gray-200 leading-8'>Developer Front-end based in Porto, Portugal.</p>
             </div>
           
-            <div className=' max-w-lg '>
-              <Image className='w-72 shadow-md rounded-full' src={nica} alt={'foto marcelo'}/>
-              </div>
+            <div className='pb-2'>
+              <Image className='md:w-72 w-48 shadow-md rounded-full' src={nica} alt={'foto marcelo'}/>
+            </div>
             
           </div> 
-          <div className='  w-3/4 flex px-60 flex-col p-20 justify-center items-center'>
+          <div className='md:w-3/4 flex md:px-60 flex-col md:p-20 p-10 justify-center items-center'>
             <Reveal>
-              <div className='text-lg border-black border-2 py-1 px-2 shadow-lg dark:text-white dark:border-white font-semibold mb-5'>ABOUT ME</div>
-                        <p className=' dark:text-gray-200 text-gray-800 md:text-md  mx-auto mb-1'>Brazilian, vivo na cidade do Porto, Portugal à 3 anos, e sou formado em Marketing. </p>
-                        <p>Estudo front end há alguns anos e tenho um pouco de conhecimento em back-end, além disso me interesso por games, robótica e deep learning.</p>
+              <div className='md:text-lg text-sm border-black border-2 py-1 px-2 shadow-lg dark:text-white dark:border-white font-semibold mb-5 '>ABOUT ME</div>
+                        <p className='dark:text-gray-200 text-gray-800 md:text-md mx-auto mb-1 '>Brazilian, vivo na cidade do Porto, Portugal à 3 anos, e sou formado em Marketing. </p>
+                        <p className='dark:text-gray-200 text-gray-800 md:text-md mx-auto mb-1'>Estudo front end há alguns anos e tenho um pouco de conhecimento em back-end, além disso me interesso por games, robótica e deep learning.</p>
             </Reveal>
             <Reveal>
-          <h4 className='text-5xl m-10 font-extrabold dark:text-white' >Skills</h4>
+          <h4 className='md:text-5xl text-3xl m-10 font-extrabold dark:text-white' >Skills</h4>
      
-                <ul className='text-6xl   grid grid-cols-3 gap-10'>
+                <ul className='md:text-6xl text-4xl  grid grid-cols-3 gap-10'>
                   <li alt='HTML'><AiFillHtml5 className='text-orange-600' name={'HTML'} alt={'HTML'}/></li>
                   <li><FaCss3Alt className='text-blue-500'/></li>
                   <li><IoLogoJavascript className='text-yellow-400'/></li>
                   <li><FaReact className='text-sky-700 '/></li>
                   <li><FaNodeJs className='text-lime-600'/></li>
-                  <li><Image src={python} className='h-16 w-16'/></li>
+                  <li><Image src={python} className='md:h-16 h-10 w-10 md:w-16'/></li>
                   <li><FaGithub className='dark:text-white'/></li>
                   <li><SiArduino className='text-teal-500'/></li>
                   <li><SiMysql className='dark:text-white'/></li>
@@ -97,13 +97,13 @@ const [darkMode, setDarkMode] = useState(false);
             </div>
 
   </section>
-  <section className='min-h-screen dark:bg-gray-700 layout px-10 md:px-20 lg:px-40 '>
+  <section className='min-h-screen  dark:bg-gray-700 layout px-10 md:px-20 lg:px-40 '>
 
     <Reveal>
-      <div className='py-20 flex'><h2 className='text-2xl shadow-md text-white font-semibold px-3 py-2 border-2 '>Projects</h2></div>
-          <div className='grid-cols-1 grid md:grid-cols-2 md:gap-10 px-20 dark:bg-gray-700 lg:grid-cols-3' >
-              <div className='mb-20 flex flex-col justify-between text-start text-sm overflow-hidden max-w-xs h-96 bg-white shadow-md dark:bg-gray-800'>
-                <div className='max-w-md'>
+      <div className='py-20 flex'><h2 className='text-2xl shadow-md text-white font-semibold px-3 py-2 border-2 '>PROJECTS</h2></div>
+          <div className='grid-cols-1 grid group md:grid-cols-2 md:gap-10 dark:bg-gray-700 lg:grid-cols-3' >
+              <div className='hover:!scale-100 duration-500 group-hover:scale-[0.95] flex flex-col justify-between text-start text-sm overflow-hidden max-w-xs h-96 bg-white shadow-md dark:bg-gray-800'>
+                <div className='max-w-md mix-blend-luminosity'>
                   <Image className='object-cover cursor-pointer' src={simon} alt={'imagem projeto simon'} />
                 </div>
                 <h3 className='text-lg px-5 font-medium p-2 dark:text-gray-200'>Simon Says!</h3>
@@ -117,7 +117,7 @@ const [darkMode, setDarkMode] = useState(false);
       
               </div>
       
-              <div className='mb-20 dark:bg-gray-800 flex flex-col justify-between text-start text-sm overflow-hidden max-w-xs h-96 bg-white shadow-xl  rounded-md'>
+              <div className='mb-20 duration-500 group-hover:scale-[0.95] hover:!scale-100 dark:bg-gray-800 flex flex-col justify-between text-start text-sm overflow-hidden max-w-xs h-96 bg-white shadow-xl'>
                 <div className='max-w-md'>
                   <Image className='object-cover w-96 cursor-pointer' src={credit} alt={'imagem projeto rocket card'} />
                 </div>
@@ -130,7 +130,7 @@ const [darkMode, setDarkMode] = useState(false);
                 </div>
       
               </div>
-              <div className='mb-20 dark:bg-gray-800 flex flex-col justify-between text-start text-sm overflow-hidden max-w-xs h-96 bg-white shadow-xl  rounded-md '>
+              <div className='mb-20 duration-500 group-hover:scale-[0.95] hover:!scale-100 dark:bg-gray-800 flex flex-col justify-between text-start text-sm overflow-hidden max-w-xs h-96 bg-white shadow-xl'>
                 <div className='max-w-md'>
                   <Image className='object-contain cursor-pointer w-96 border-gray-400' src={movies} alt={'imagem projeto movies'} />
                 </div>
@@ -148,10 +148,10 @@ const [darkMode, setDarkMode] = useState(false);
         </section> 
         <section className='min-h-screen layout flex flex-col justify-between dark:bg-gray-700  '>
 
-          <div className=' flex flex-row justify-around items-center z-10'>
+          <div className=' flex md:flex-row flex-col md:justify-around items-center z-10'>
             <div className='pt-60'>
               <Reveal>
-                <h2 className='text-white font-semibold mt-30 mb-10 dark:text-yellow-400'><span className='border-2 px-3 py-2 shadow-md border-white'>CONTACT</span></h2>
+                <h2 className='text-white font-semibold mt-30 mb-10'><span className='border-2 px-3 py-2 shadow-md border-white'>CONTACT</span></h2>
                 <h3 className='text-7xl font-bold dark:text-white'>Let's talk</h3>
                 <div className=' text-4xl flex justify-center gap-16 py-5 text-white  dark:text-gray-900'>
                             <AiFillLinkedin className='cursor-pointer hover:text-black'/>
@@ -162,7 +162,7 @@ const [darkMode, setDarkMode] = useState(false);
               </Reveal>
             </div>
             <Fade left>
-              <div id='contact' className='relative top-48 right-24 bg-gray-400 dark:bg-gray-500 '>
+              <div id='contact' className='relative top-24 md:top-48 md:right-24 bg-gray-400 dark:bg-gray-500 '>
                 <h2 className='yb-5 py-5 pl-2 bg-gray-500 dark:bg-black font-bold text-shadow-md text-white  text-lg'>Send me a Message</h2>
                 <ContactUs/>
               </div>
